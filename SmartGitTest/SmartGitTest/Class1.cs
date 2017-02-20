107bee6994a001aebbe342143728db08d8cffb06
SuJinbiao-kooboo <jinba@kooboo.cn>
2017-02-20 17:03:10 +0800

    +

--- SmartGitTest/SmartGitTest/Class1.cs
+++ SmartGitTest/SmartGitTest/Class1.cs
@@ -8,6 +8,9 @@ namespace SmartGitTests
 {
     public class Class1
     {
-        public int Age { get; set; }
+        public int Age { get; set; }\
+
+
+        public string Name { get; set; }
     }
 }
